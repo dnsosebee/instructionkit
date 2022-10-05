b2952b1b1e6f3c8ae475166092fcf465b39d1f36
Daniel Sosebee <dnsosebee@gmail.com>
2022-10-05 10:48:31 -0700

    minor things

---
+++ src/model/flowpad/selection.ts
@@ -0,0 +1,11 @@
+import { proxy } from "valtio";
+import { Dart } from "../core/floem";
+import { Flow } from "../core/flow";
+
+type Selectable = Flow | Dart | null;
+
+type FlowpadState = { selected: Selectable };
+
+const flowpadState = proxy<FlowpadState>({
+  selected: null,
+});
