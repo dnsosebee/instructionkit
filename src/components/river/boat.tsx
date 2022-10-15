bc4bdcdfcba933d2b561135f77a0327c528c97a8
Daniel Sosebee <dnsosebee@gmail.com>
2022-10-15 12:46:43 -0700

    with valtio, broken

--- src/components/river/boat.tsx
+++ src/components/river/boat.tsx
@@ -1,45 +1,21 @@
 import { trim } from 'lodash'
 import { HTMLElement, NodeType, parse } from 'node-html-parser'
-import { DataDart } from '../../model/core/floem'
+import { DataDart, DataFloem } from '../../model/core/floem'
 import { DataFlow } from '../../model/core/flow'
-import { Stone } from './stone'
+import { NextButton } from './gaps'
+import { FlowState, StoneType } from './river'
 
-const STONE_SEPARATOR = '...'
-const RIFFLE_SEPARATOR = '<hr>'
-const INPUT_REGEX = /hello world/
-
-export type CallbackType = (advance: () => Stone) => () => void
-
-export interface EmbarkData {
-  flows: DataFlow[]
-  darts: DataDart[]
-  callback: CallbackType
+interface FullFlowState extends FlowState {
+  flowNodes: HTMLElement[]
 }
 
-export const embark = (data: EmbarkData) => {
-  console.log('Embarking on an epic riverine journey')
-  return newStone({
-    ...data,
-    flocation: 'flow-start',
-    hopper: refill(data.flows, 'flow-start'),
-    fragment: { paddle: true, children: [] },
-  })
-}
-
-export interface NewStoneData extends EmbarkData {
-  flocation: DataFlow['id']
-  hopper: HTMLElement[]
-  fragment: Fragment
-}
-
-interface Fragment {
-  paddle: boolean
-  children: HTMLElement[]
-}
-
-export interface Stone {
-  paddle: boolean // true if the stone is the beginning of a riffle
-  element: JSX.Element
+export function stoneAt(floem: DataFloem, flowState: FlowState): StoneType {
+  return helper(
+    floem.flows,
+    floem.darts,
+    { ...flowState, flowNodes: refill(floem.flows, flowState.flocation.flow) },
+    [],
+  )
 }
 
 const toJsx = (elements: HTMLElement[]) => (
@@ -55,51 +31,63 @@ const refill = (flows: DataFlow[], id: DataFlow['id']): HTMLElement[] => {
   return Array.from(childElements)
 }
 
-const newStone = (data: NewStoneData): Stone => {
-  const { flows, darts, fragment } = data
-
-  if (data.hopper.length === 0) {
-    const branches = darts.filter(v => v.from == data.flocation)
+const helper = (
+  flows: DataFlow[],
+  darts: DataDart[],
+  flowState: FullFlowState,
+  fragment: HTMLElement[],
+): StoneType => {
+  if (flowState.flocation.node >= flowState.flowNodes.length) {
+    const branches = darts.filter(v => v.from == flowState.flocation.flow)
     if (branches.length === 0) {
       return {
-        paddle: fragment.paddle,
-        element: toJsx(fragment.children),
+        value: null,
+        flowState: () => flowState,
+        view: () => <>{fragment.map(v => v)}</>,
+        paddleAfter: false,
       }
+      const dart = branches[0] // TODO flogic
+      const flowId = dart.to
+      const flowNodes = refill(flows, flowId)
+      return helper(
+        flows,
+        darts,
+        {
+          ...flowState,
+          flocation: {
+            flow: flowId,
+            node: 0,
+          },
+          flowNodes,
+        },
+        fragment,
+      )
     }
-    const dart = branches[0] // TODO flogic
-    const flocation = dart.to
-    const hopper = refill(flows, flocation)
-    return newStone({ ...data, flocation, hopper })
   }
 
-  const el = data.hopper.shift()!
+  const el = flowState.flowNodes[flowState.flocation.node]
+  const nextFlowState = {
+    ...flowState,
+    flocation: {
+      ...flowState.flocation,
+      node: flowState.flocation.node + 1,
+    },
+  }
   let match
-
-  console.log('el', el.rawText)
 
   if (el.tagName === 'HR') {
     return {
-      paddle: fragment.paddle,
-      element: (
-        <Stone>
-          <>
-            {toJsx(fragment.children)}
-            <NextButton {...data} fragment={{ children: [], paddle: true }} />
-          </>
-        </Stone>
-      ),
+      value: null,
+      flowState: () => nextFlowState,
+      paddleAfter: true,
+      view: NextButton(true, toJsx(fragment)),
     }
   } else if (el.tagName === 'P' && (match = el.innerText?.match(/^(...)|…$/g))) {
     return {
-      paddle: fragment.paddle,
-      element: (
-        <Stone>
-          <>
-            {toJsx(fragment.children)}
-            <NextButton {...data} fragment={{ children: [], paddle: false }} />
-          </>
-        </Stone>
-      ),
+      value: null,
+      flowState: () => nextFlowState,
+      paddleAfter: false,
+      view: NextButton(false, toJsx(fragment)),
     }
   } else if (
     el.tagName === 'p' &&
@@ -109,45 +97,13 @@ const newStone = (data: NewStoneData): Stone => {
   ) {
     const choices = match.groups!.choices.split(',').map(trim)
     return {
-      paddle: fragment.paddle,
-      element: (
-        <Stone>
-          <>
-            {toJsx(fragment.children)}
-            <GapChoice
-              data={{ ...data, fragment: { children: [], paddle: false } }}
-              assignment={match.groups!.assignment}
-              choices={choices}
-            />
-          </>
-        </Stone>
-      ),
+      value: null,
+      flowState: () => nextFlowState,
+      paddleAfter: false,
+      view: NextButton(false, toJsx(fragment)),
     }
   }
 
-  fragment.children.push(el)
-  return newStone(data)
-}
-
-const NextButton = (data: NewStoneData) => {
-  const { callback } = data
-  return <button onClick={callback(() => newStone(data))}>next</button>
-}
-
-interface ChoiceProps {
-  assignment: string | undefined
-  choices: string[]
-  data: NewStoneData
-}
-
-const GapChoice = ({ data, assignment, choices }: ChoiceProps) => {
-  console.log(assignment)
-  const { callback } = data
-  return (
-    <span>
-      {choices.map(v => (
-        <button onClick={callback(() => newStone(data))}>{v}</button>
-      ))}
-    </span>
-  )
+  fragment.push(el)
+  return helper(flows, darts, nextFlowState, fragment)
 }
--- src/components/river/river.tsx
+++ src/components/river/river.tsx
@@ -1,37 +1,132 @@
-import { List } from 'immutable'
-import { useEffect, useState } from 'react'
+import { List, Map } from 'immutable'
+import { useEffect } from 'react'
+import { proxy, snapshot, useSnapshot } from 'valtio'
 import { DataFloem } from '../../model/core/floem'
+import { DataFlow } from '../../model/core/flow'
 import { Mutate } from '../../model/core/mutators'
-import { CallbackType, embark, Stone } from './boat'
+import { stoneAt } from './boat'
 
 interface RiverProps {
   mutate: Mutate
   floem: DataFloem
 }
 
-export type Riffle = List<JSX.Element>
+type LValue = string
+type RValue = any
+type VarMap = Map<LValue, RValue>
 
-export const River = ({ mutate, floem }: RiverProps) => {
-  const [riffles, setRiffles] = useState<List<Riffle>>(List([List()]))
+export interface FlowState {
+  vars: VarMap
+  flocation: {
+    flow: DataFlow['id']
+    node: number
+  }
+}
 
-  const updateRiver = ({ paddle, element }: Stone) => {
-    setRiffles(riffles => {
-      const newRiffles = paddle ? riffles.push(List()) : riffles
-      return newRiffles.set(-1, newRiffles.last()!.push(element))
+export interface StoneProps {
+  active: boolean
+  value: any
+  onHop: (value: any) => void
+}
+
+interface Stone<T> {
+  value: T
+  flowState: (value: any) => FlowState
+  view: (props: StoneProps) => JSX.Element
+  paddleAfter: boolean
+}
+const WELCOME_STONE: Stone<null> = {
+  value: null,
+  flowState: () => ({
+    vars: Map(),
+    flocation: {
+      flow: 'flow-start',
+      node: 0,
+    },
+  }),
+  view: ({ active, onHop }) => {
+    if (!active) {
+      return <></>
+    }
+    return (
+      <div>
+        <h1>Welcome to the river</h1>
+        <button onClick={() => onHop(null)}>Start</button>
+      </div>
+    )
+  },
+  paddleAfter: true,
+}
+
+export type StoneType = Stone<string> | Stone<number> | Stone<boolean> | Stone<null>
+
+export type Riffle = List<StoneType>
+
+// setup valtio state
+const state = proxy({
+  riffleList: {
+    riffles: List<Riffle>([List<StoneType>([WELCOME_STONE] as StoneType[])] as Riffle[]),
+    activeRiffle: 0,
+  },
+})
+
+const rewindAndApply = (
+  riffleIdx: number,
+  stoneIdx: number,
+  value: any,
+  paddleAfter: boolean,
+  floem: DataFloem,
+) => {
+  const riffleList = state.riffleList
+  const riffle = riffleList.riffles.get(riffleIdx)!
+  const stone = riffle.get(stoneIdx)!
+  const updatedRiffle = riffle
+    .set(stoneIdx, {
+      ...stone,
+      value,
     })
+    .slice(0, stoneIdx + 1)
+  let updatedRiffles = riffleList.riffles.set(riffleIdx, updatedRiffle).slice(0, riffleIdx + 1)
+  let updatedActiveRiffle = riffleList.activeRiffle
+  const newStone = stoneAt(floem, stone.flowState(value))
+  if (paddleAfter) {
+    const newRiffle = List<StoneType>([newStone])
+    updatedRiffles = updatedRiffles.push(newRiffle)
+    updatedActiveRiffle = updatedRiffles.size - 1
+  } else {
+    updatedRiffles = updatedRiffles.set(riffleIdx, updatedRiffle.push(newStone))
   }
-
-  const callback: CallbackType = advance => () => {
-    updateRiver(advance())
+  state.riffleList = {
+    riffles: updatedRiffles,
+    activeRiffle: updatedActiveRiffle,
   }
+  console.log('rewindAndApply', snapshot(state))
+}
 
-  useEffect(() => updateRiver(embark({ flows: floem.flows, darts: floem.darts, callback })), [])
+export const River = ({ floem }: RiverProps) => {
+  const { riffles, activeRiffle } = useSnapshot(state.riffleList)
 
+  useEffect(() => {
+    console.log('River useEffect', riffles, activeRiffle)
+  }, [riffles, activeRiffle])
   return (
-    <div className=' flex flex-col h-full prose'>
-      {riffles.last()!.map((element, i) => (
-        <div key={i} className=''>
-          {element}
+    <div>
+      <p>{activeRiffle}</p>
+      {riffles.map((riffle, riffleIdx) => riffle.map(v => <p>{v.value}</p>))}
+      {riffles.map((riffle, i) => (
+        <div key={i}>
+          {riffle.map((stone, j) => (
+            <stone.view
+              key={j}
+              {...{
+                active: i === activeRiffle && j === riffle.size - 1,
+                value: stone.value,
+                onHop: value => {
+                  rewindAndApply(i, j, value, stone.paddleAfter, floem)
+                },
+              }}
+            />
+          ))}
         </div>
       ))}
     </div>
