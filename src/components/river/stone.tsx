04e3f964691da8b8b6c12ea7997e38432c4a6935
Christopher Walker <cwalker@polytrope.com>
2022-10-17 10:49:45 -0700

    Styles

--- src/components/river/stone.tsx
+++ src/components/river/stone.tsx
@@ -34,7 +34,7 @@ const NextButton = (params: NextButtonParams) => (props: AdvancerProps) => {
     active && (
       <button
         disabled={!active}
-        className='inline-flex items-center rounded-md border border-gray-300 bg-white mt-3 px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 self-center cursor-pointer'
+        className='inline-flex items-center rounded-md border border-gray-300 bg-white mt-5 px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 self-center cursor-pointer'
         onClick={() => onHop(null)}
       >
         {params.text}
@@ -55,7 +55,7 @@ const Choice = (params: ChoiceParams) => (props: AdvancerProps) => {
         <button
           disabled={!active}
           type='button'
-          className={`relative -ml-px inline-flex items-center border border-gray-300 bg-white mt-3 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 cursor-pointer ${
+          className={`relative -ml-px inline-flex items-center border border-gray-300 bg-white mt-5 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 cursor-pointer ${
             i == 0 ? 'rounded-l-md' : ''
           } ${i == params.choices.length - 1 ? 'rounded-r-md' : ''}`}
           onClick={() => onHop(choice)}
