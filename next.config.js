a35c640f0b53d8cb72780c894abb58b57eb70866
Daniel Sosebee <dnsosebee@gmail.com>
2022-10-18 17:16:26 -0700

    move dotenv to dev dep

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
-const webpack = require('webpack')
-const { parsed: myEnv } = require('dotenv').config({
-  path: './.env',
-})
-
 module.exports = {
-  webpack: config => {
-    config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
-    return config
-  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
