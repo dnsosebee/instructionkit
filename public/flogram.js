69e9bb1759c8f96717a43c5b9a312b7507b1ae82
Daniel Sosebee <dnsosebee@gmail.com>
2022-10-26 14:37:46 -0700

    add better flogramming

--- public/flogram.js
+++ public/flogram.js
@@ -1,18 +1,13 @@
 /* eslint-disable no-global-assign */
-/* global self, onmessage, postMessage */
+/* global self, onmessage, postMessage, console */
 onmessage = function (e) {
   const { vars, flogram } = e.data
-  const prevKeys = Object.keys(self)
-  console.log('prevKeys', prevKeys)
+  const prevEntries = Object.entries(self).map(([key, value]) => [key, value])
   Object.entries(vars).forEach(([key, value]) => {
     self[key] = value
   })
-  const newKeys = Object.keys(self)
-  console.log('newKeys', newKeys)
   eval(flogram)
   postMessage(
-    Object.entries(self).filter(
-      ([key, value]) => !prevKeys.includes(key) && typeof value !== 'function',
-    ),
+    Object.entries(self).filter(v => !prevEntries.includes(v) && typeof v[1] !== 'function'),
   )
 }
