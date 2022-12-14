0e69cf0dfe9c83d9423a20513ad3c4653929aeca
Christopher Walker <15720465+polytroper@users.noreply.github.com>
2022-12-14 11:44:29 -0800

    Improve responsiveness of groups

--- src/model/tiptap/groupNode.ts
+++ src/model/tiptap/groupNode.ts
@@ -33,7 +33,7 @@ const GroupNode = Node.create({
     return [
       'div',
       mergeAttributes(HTMLAttributes, {
-        class: `group ${HTMLAttributes.row ? 'flex row flex-wrap' : 'column'}`,
+        class: `group basis-64 grow ${HTMLAttributes.row ? 'flex row flex-wrap' : 'column'}`,
       }),
       0,
     ]
