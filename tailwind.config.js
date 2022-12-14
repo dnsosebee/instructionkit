99bf42fabae17f963df63b963cfd8e3f09b1b152
Christopher Walker <15720465+polytroper@users.noreply.github.com>
2022-12-13 20:24:56 -0800

    Add not-first to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
   plugins: [
     plugin(function ({ addVariant }) {
       addVariant('not-last', '&:not(:last-child)')
+      addVariant('not-first', '&:not(:first-child)')
     }),
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
