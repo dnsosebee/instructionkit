3b05b3beb3e72469d5ef553085daee1830da30f2
Christopher Walker <15720465+polytroper@users.noreply.github.com>
2022-12-14 11:13:45 -0800

    Add bottom margin to assignee node

--- src/components/floem/tiptap/assignee.tsx
+++ src/components/floem/tiptap/assignee.tsx
@@ -7,7 +7,7 @@ export const Assignee = (props: NodeViewProps) => {
   const isChart = context.view === View.Flowchart
 
   return isChart ? (
-    <NodeViewWrapper as='div' className='flex'>
+    <NodeViewWrapper as='div' className='flex mb-2'>
       <NodeViewContent className='text-center text-l33t px-2' as={'div'} />
       <div contentEditable={false} className='text-center pointer-events-none px-2'>
         =
