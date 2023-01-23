ec5ba66f0f19916d138ec675d2eccbc0aebfb0c9
Christopher Walker <15720465+polytroper@users.noreply.github.com>
2023-01-23 14:41:02 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ module.exports = {
     return [
       {
         // matching all guide routes
-        source: '/:path*',
+        source: '/guide/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
